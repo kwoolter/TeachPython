5f8f48531a9e99d1ffe2a762c3b0fc11c9da7d4b
keith <keith.woolterton@gmail.com>
2023-04-01 08:32:15 +0100

    Tidy up menu

--- teachpy/menu.py
+++ teachpy/menu.py
@@ -1,13 +1,15 @@
 import cmd
-import teachpy_utils as utils
+from . import teachpy_utils as utils
+
 
 def run():
-
     c = TeachPyCLI()
     c.run()
 
 
 class TeachPyCLI(cmd.Cmd):
+    """ Command Line interface to run teh Tech Python menu"""
+
     def __init__(self):
 
         self.intro = "Welcome to Teach Python.\nType help for assistance."
@@ -15,28 +17,47 @@ class TeachPyCLI(cmd.Cmd):
 
         super(TeachPyCLI, self).__init__()
 
+        return
 
-    def do_start(self, arg : str):
+    def do_start(self, arg: str):
         """Start a module"""
 
-        modules = {1:"Basic Stuff", 2:"Getting going", 3:"Heating up!", 4:"On fire!!!"}
+        modules = ("Basic Stuff", "Getting going", "Heating up!", "On fire!!!")
 
         if len(arg) == 0:
             print("You didn't specify a module to start!")
 
-            selection = utils.pick("Module", list(modules.values()))
+            selection = utils.pick("Module", modules)
+            module_number = modules.index(selection) + 1
 
-            utils.type_text(f"Starting {selection}...\n")
+            utils.type_text(f"Starting module {module_number}. {selection}...\n")
+
 
         else:
-            print(f"Starting module {arg[0]}...")
+            selection = arg
+
+            try:
+                module_number = modules.index(arg) + 1
+                utils.type_text(f"Starting module {module_number}. {selection}...\n")
+
+            except ValueError:
+                print(f"Module '{selection}' not found")
+                return
+
+        if module_number == 1:
+            from . import module1
+            pass
+        elif module_number == 2:
+            from . import module2
+
+        return
 
     def run(self):
+
         self.cmdloop()
+
+        return
 
 
 if __name__ == "__main__":
     run()
-
-
-
